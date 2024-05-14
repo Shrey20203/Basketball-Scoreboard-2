e5e82f88da0c852c0d77f451ae19a6fdb7c5e4e1
Shrey <kas.shrey@gmail.com>
2024-05-14 16:13:09 +0530

    Update js

--- index.js
+++ index.js
@@ -9,32 +9,32 @@ const guestScore = document.querySelector("#guest-score")
 let totalHomeScore = 0
 let totalGuestScore = 0
 
-homeScoreBtnOne.addEventListener("click", function (){
+homeScoreBtnOne.addEventListener("click", function() {
     totalHomeScore += 1
     homeScore.textContent = totalHomeScore
 })
 
-homeScoreBtnTwo.addEventListener("click", function (){
+homeScoreBtnTwo.addEventListener("click", function() {
     totalHomeScore += 2
     homeScore.textContent = totalHomeScore
 })
 
-homeScoreBtnThree.addEventListener("click", function (){
+homeScoreBtnThree.addEventListener("click", function() {
     totalHomeScore += 3
     homeScore.textContent = totalHomeScore
 })
 
-guestScoreBtnOne.addEventListener("click", function (){
+guestScoreBtnOne.addEventListener("click", function() {
     totalGuestScore += 1
     guestScore.textContent = totalGuestScore
 })
 
-guestScoreBtnTwo.addEventListener("click", function (){
+guestScoreBtnTwo.addEventListener("click", function() {
     totalGuestScore += 2
     guestScore.textContent = totalGuestScore
 })
 
-guestScoreBtnThree.addEventListener("click", function (){
+guestScoreBtnThree.addEventListener("click", function() {
     totalGuestScore += 3
     guestScore.textContent = totalGuestScore
 })
